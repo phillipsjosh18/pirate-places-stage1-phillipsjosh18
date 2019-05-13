747953200e55d2dd300c00d01cdcee3e4fb7b9c0
Mark Hills <mhills@acm.org>
2019-05-12 22:00:50 -0400

    Initial version of Homework 1 code

---
+++ app/src/main/java/edu/ecu/cs/pirateplaces/PiratePlacesActivity.java
@@ -0,0 +1,15 @@
+package edu.ecu.cs.pirateplaces;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PiratePlacesActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pirate_places);
+    }
+}
